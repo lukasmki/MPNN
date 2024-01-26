593a3fb68662af9568f3b999cf1966c15f8a2eb6
lukasmki <lukasarus@gmail.com>
2024-01-26 13:11:06 -0800

    Remove old models

--- mpnn/models/__init__.py
+++ mpnn/models/__init__.py
@@ -1,7 +1,2 @@
 from mpnn.models.newtonnet import NewtonNet
-from mpnn.models.paulingnet import PaulingNet
 from mpnn.models.mpnn import MPNN
-from mpnn.models.mayernet import MayerNet
-from mpnn.models.genmp import GENMP
-from mpnn.models.reaxML import ReaxML
-from mpnn.models.reboML import ReboML
